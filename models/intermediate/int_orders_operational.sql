27d9b440ed71358d12124307a596565aa87ad941
rozo.yannick@gmail.com <rozo.yannick@gmail.com>
2024-05-07 13:15:21 +0000

    operational margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,8 @@
+SELECT 
+    orders_id
+    ,date_date
+    ,ROUND(margin+shipping_fee-logcost-ship_cost,2) AS operational_margin
+FROM {{ ref('int_orders_margin') }}
+LEFT JOIN {{ ref('stg_raw__ship') }}
+    USING(orders_id)
+ORDER BY orders_id DESC
